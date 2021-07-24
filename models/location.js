1443ec5ad8f91331a21598265fb52dfb4b5f40cd
bbibbibiq <peerasorn14@gmail.com>
2021-07-24 22:50:18 +0700

    Remove _id key

--- models/location.js
+++ models/location.js
@@ -3,17 +3,18 @@ const Schema = mongoose.Schema
 
 const LocationSchema = new Schema({
   slug: String,
-  items: {
-    _id: Schema.Types.ObjectId,
-    slug: String,
-    name: String,
-    image: String,
-    price: String,
-    quantity: {
-      total: Number,
-      remaining: Number
+  items: [
+    {
+      slug: String,
+      name: String,
+      image: String,
+      price: String,
+      quantity: {
+        total: Number,
+        remaining: Number
+      }
     }
-  }
+  ]
 })
 
 module.exports = mongoose.model('Location', LocationSchema)
