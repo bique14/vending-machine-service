26c8095df027a22bfe33af44461c7ddb98c1c37c
bbibbibiq <peerasorn14@gmail.com>
2021-07-25 20:35:47 +0700

    allow cors

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const bodyParser = require('koa-bodyparser')
 const Koa = require('koa')
+const cors = require('@koa/cors')
+
 const { NODE_PORT } = require('./config')
 const { admin, client } = require('./routes')
 const { connect } = require('./services')
+
 const app = new Koa()
 
 connect()
 
+app.use(cors())
 app.use(bodyParser())
 app.use(admin.routes())
 app.use(admin.allowedMethods())
